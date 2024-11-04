cbf308f17bed7cca56bf40dd4a10662d5fc914e3
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2024-11-04 15:47:02 -0300

    New

---
+++ 3º Trimestre/POO/composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'usuario/composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0104a6355022f329a63a9dd101e5cbbd725114a4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'usuario/composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0104a6355022f329a63a9dd101e5cbbd725114a4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
